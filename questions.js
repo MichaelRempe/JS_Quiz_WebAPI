a22005d7b5d52544e0defc14f3fa2da76901c0d5
MichaelRempe <michaeljrempe@gmail.com>
2020-01-06 11:01:10 -0500

    JSON Questions Generated, Inital Page Load Format

---
+++ questions.js
@@ -0,0 +1,26 @@
+var querstions = [
+    {
+        title: "Primitive data types in JS do NOT include:",
+        options: ["String", "NULL", "Undefined", "Objects"],
+        answer: "Objects"
+    }, {
+        title: "This boolean notation denotes both equality in type AND value",
+        options: [".equals", "&&", "===", "!="],
+        answer: "==="
+    },
+    {
+        title: "This is the notation for accecing JS object data:",
+        options: ["var data = object.key","var data = object.value","var data = object.type","var object.get(value)"],
+        answer: "var data = object.key"
+    },
+    {
+        title: "JSON stands for:",
+        options: ["JavaScript Organization Network","JavaScript Object Notation", "JavaScript Orientation Notes", "JavaScript Opperand Numerics"],
+        answer: "JavaScript Object Notation"
+    },
+    {
+        title: "The highest top-level vinilla JavaScript object is the:",
+        options: ["<HTML> tag","Window", "Web-Server", "Document"],
+        answer: "Window"
+    }
+]
